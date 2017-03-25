d48673ee36c523b95f14f67fd06c58c1f782c6ad
lidejian@skio.cn <lidejian>
2017-03-25 22:14:50 +0800

    数据库地址

--- config/puma.rb
+++ config/puma.rb
@@ -14,11 +14,11 @@ application_path = "/var/www/#{app_name}" if Rails.env.production?
 directory "#{application_path}/"
 p application_path
 #下面都是 puma的配置项
-pidfile "#{application_path}/tmp/pids/puma.pid"
-state_path "#{application_path}/tmp/sockets/puma.state"
-stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
-bind "unix://#{application_path}/tmp/sockets/#{app_name}.sock"
-activate_control_app "unix://#{application_path}/tmp/sockets/pumactl.sock"
+# pidfile "#{application_path}/tmp/pids/puma.pid"
+# state_path "#{application_path}/tmp/sockets/puma.state"
+# stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
+# bind "unix://#{application_path}/tmp/sockets/#{app_name}.sock"
+# activate_control_app "unix://#{application_path}/tmp/sockets/pumactl.sock"
 
 #后台运行
 # daemonize true
